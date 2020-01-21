187ff2239c0b250043c68453f0aef936c1be8778
noumanot <nouman.mustafa@otif-solutions.com>
2020-01-21 12:20:07 +0500

    changed sidebar new with design

--- dist/main.js
+++ dist/main.js
@@ -3,3 +3,24 @@ $('.openBtn').on('click',function(){
         $('#myModal').modal({show:true});
     });
 });
+
+function switchVisible() {
+    if (document.getElementById('FullSidebarColumn')) {
+
+        if (document.getElementById('FullSidebarColumn').style.display == 'none') {
+            document.getElementById('FullSidebarColumn').style.display = '';
+            document.getElementById('CollapsedSidebarColumn').style.display = 'none';
+            $('.home__right').animate({
+                marginLeft: '25em'
+            },500)
+        }
+        else {
+            $('.home__right').animate({
+                marginLeft: '8em'
+            },500)
+        
+            document.getElementById('FullSidebarColumn').style.display = 'none';
+            document.getElementById('CollapsedSidebarColumn').style.display = 'block';
+        }
+    }
+}
